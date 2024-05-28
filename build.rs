4c62898b6efbd3708ffbf4ca0b98a17e97a96036
LeoniePhiline <22329650+LeoniePhiline@users.noreply.github.com>
2024-05-28 11:45:19 +0200

    feat(ui): set GUI style

--- build.rs
+++ build.rs
@@ -1,3 +1,7 @@
 fn main() {
-    slint_build::compile("ui/appwindow.slint").unwrap();
+    slint_build::compile_with_config(
+        "ui/appwindow.slint",
+        slint_build::CompilerConfiguration::new().with_style(String::from("fluent-dark")),
+    )
+    .unwrap();
 }
